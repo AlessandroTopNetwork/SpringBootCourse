604f97742d4561dc990837a72162130361502b0c
AlessandroTopNetwork <>
2024-04-11 18:07:36 +0200

    permission endpoint by role

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-access-endpoint-by-role-account-data-moked-in-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,75 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+	
+	private String basePath = "/api/employees";
+	
+	// add support for jdbc . . . no more hard code user :-)
+	
+	@Bean
+	public UserDetailsManager userDetailManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // add check role of user login to access the many endpoint
+		
+		http.authorizeHttpRequests(configurer ->
+				configurer
+					.requestMatchers(HttpMethod.GET, basePath).hasRole("EMPLOYEE")
+					.requestMatchers(HttpMethod.GET, basePath + "/**").hasRole("EMPLOYEE") // all path after base path
+					.requestMatchers(HttpMethod.POST, basePath).hasRole("MANAGER")
+					.requestMatchers(HttpMethod.PUT, basePath).hasRole("MANAGER")
+					.requestMatchers(HttpMethod.DELETE, basePath + "/**").hasRole("ADMIN")
+				);
+		
+		// use HTTP Basic authentication
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		// disable Cross Site Request Forgery (CSRF)
+		// in general , not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+		
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+	
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailManager() { // add simple list of user to login for call endpoitn of EmployeeRestCOntroller
+//		
+//		UserDetails john = User.builder().
+//				username("john").
+//				password("{noop}test123").
+//				roles("EMPLOYEE").
+//				build();
+//		
+//		UserDetails mary = User.builder().
+//				username("mary").
+//				password("{noop}test123").
+//				roles("EMPLOYEE", "MANAGER").
+//				build();
+//		
+//		UserDetails susan = User.builder().
+//				username("susan").
+//				password("{noop}test123").
+//				roles("EMPLOYEE", "MANAGER", "ADMIN").
+//				build();
+//		
+//		return new InMemoryUserDetailsManager(john, mary, susan);
+//		
+//	}
+
+}
