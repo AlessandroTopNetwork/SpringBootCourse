a5914a8c432b06df84d2f3f64fb58ee950343aa0
AlessandroTopNetwork <>
2024-04-30 11:12:02 +0200

    custom login page thymelaft  springboot security

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -36,6 +38,25 @@ public class DemoSecurityConfig {
 		
 		return new InMemoryUserDetailsManager(Arrays.asList(jhon, mary, susan));
 		
+	}
+	
+	// custom login form
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(configurer ->
+				configurer
+						.anyRequest().authenticated()
+				)
+				.formLogin(form ->
+						form
+						.loginPage("/showMyLoginPage") // page in the dir template to login page call it by endpoint in the controller
+						.loginProcessingUrl("/authenticateTheUser") // payh of method to process authenticate user login form will be free by spring boot
+						.permitAll()
+				);
+		return http.build();
+	
 	}
 
 }
