371a2cbdddfb2fe234179eb77b52a2f6b838b5c6
AlessandroTopNetwork <>
2024-05-06 12:49:38 +0200

    retrive lazy one to many
    (need lunch query manualy to retrieve sub list or obj)

---
+++ 09-spring-boot-jpa-advanced-mapping/03-one-to-many-retrive-data-lazy-list/src/main/java/com/onetomany/demo/dao/AppDAO.java
@@ -0,0 +1,36 @@
+package com.onetomany.demo.dao;
+
+import java.util.List;
+
+import com.onetomany.demo.entity.Course;
+import com.onetomany.demo.entity.Instructor;
+import com.onetomany.demo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+    
+    List<Course> findCoursesByIstructorId(int theIdIstructor);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
