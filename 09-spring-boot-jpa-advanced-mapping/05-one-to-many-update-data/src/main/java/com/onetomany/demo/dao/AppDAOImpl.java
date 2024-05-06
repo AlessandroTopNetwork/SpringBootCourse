e2f39b014dd2daa170a4afc7bab1d182c3f089ca
AlessandroTopNetwork <>
2024-05-06 15:09:36 +0200

    update instructor

---
+++ 09-spring-boot-jpa-advanced-mapping/05-one-to-many-update-data/src/main/java/com/onetomany/demo/dao/AppDAOImpl.java
@@ -0,0 +1,108 @@
+package com.onetomany.demo.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.onetomany.demo.entity.Course;
+import com.onetomany.demo.entity.Instructor;
+import com.onetomany.demo.entity.InstructorDetail;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        // delete the instructor
+        entityManager.remove(tempInstructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+
+        // retrieve instructor detail
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        // remove the associated object reference
+        // break bi-directional link
+        //
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+        // delete the instructor detail
+        entityManager.remove(tempInstructorDetail);
+    }
+
+	@Override
+	public List<Course> findCoursesByIstructorId(int theIdIstructor) {
+		TypedQuery<Course>  query = entityManager.createQuery(
+				"from Course where instructor.id = :data", Course.class // select * is ommision string query and secondo arguments is class of entity to retrieve
+				);
+		
+		query.setParameter("data", theIdIstructor); // set value of param into query thath name is first paramiter ("data")
+		
+		return query.getResultList();
+	}
+
+	@Override
+	public Instructor findInstructorByIdFetchJoin(int theId) { // same jps find by id
+		TypedQuery<Instructor>  query = entityManager.createQuery(
+				"select i from Instructor i " // alias instructor is i Instructor same name of class not table
+				+ " join fetch i.courses" // JOIN FETCH to retrieve istructor and it list courses
+				+ " join fetch i.instructorDetail"  // to lunch unce query to retrieve all from once query athor ways spring when call this query whitout join instructor detail after launch anhoter query to retrive instructortDetail relationated
+				+ " where i.id= :data", Instructor.class
+				);
+		
+		query.setParameter("data", theId);
+		
+		return query.getSingleResult();
+	}
+
+	@Override
+	@Transactional
+	public void update(Instructor instructor) {
+		entityManager.merge(instructor); // update in case id is popolated , insert if id is null or 0
+	}
+}
+
+
+
+
+
+
+
