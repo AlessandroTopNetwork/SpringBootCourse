c073975168b9ccf31f8bd018ca5315b60d1ca49f
AlessandroTopNetwork <>
2024-05-10 17:46:51 +0200

    catch Exception into @Aspect

---
+++ 10-springboot-aop/10-springboot-aop-demo-after-exception/src/main/java/com/aop/demo/DemoApplication.java
@@ -0,0 +1,81 @@
+package com.aop.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.aop.demo.dao.AccountDAO;
+import com.aop.demo.dao.MembershipDAO;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		return runner -> {
+
+			// demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			demoTheAfterReturningAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call method to find the accounts
+		List<Account> theAccounts = new ArrayList<Account>();
+		try {
+			theAccounts = theAccountDAO.findAccounts(false);
+		} catch (Exception e) {
+			System.out.println("never print it");
+		}
+
+		// display the accounts
+		System.out.println("\n\nMain Program: demoTheAfterReturningAdvice");
+		System.out.println("----");
+
+		System.out.println(theAccounts);
+
+		System.out.println("\n test cathc exception afte launch it\n");
+		
+		try {
+			theAccountDAO.findAccounts(true);
+		} catch (Exception e) {
+			System.out.println("test exception");
+		}
+		
+		
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		// call the business method
+		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+
+		// call the accountdao getter/setter methods
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+	}
+
+}
---
+++ 10-springboot-aop/10-springboot-aop-demo-after-exception/src/main/java/com/aop/demo/dao/AccountDAO.java
@@ -0,0 +1,24 @@
+package com.aop.demo.dao;
+
+import com.aop.demo.Account;
+
+import java.util.List;
+
+public interface AccountDAO {
+
+    // add a new method: findAccounts()
+
+    List<Account> findAccounts(boolean luchException) throws Exception;
+
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getServiceCode();
+
+    public void setServiceCode(String serviceCode);
+}
