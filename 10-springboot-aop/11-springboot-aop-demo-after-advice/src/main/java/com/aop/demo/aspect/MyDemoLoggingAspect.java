c1e2053e1d3fad83b6b48c20df4b748fbd9e22a6
AlessandroTopNetwork <>
2024-05-13 10:05:36 +0200

    after advice

---
+++ 10-springboot-aop/11-springboot-aop-demo-after-advice/src/main/java/com/aop/demo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,125 @@
+package com.aop.demo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.aop.demo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@After("execution(* com.aop.demo.dao.AccountDAO.findAccounts(..))") // after exectuion method into this package and it method findAccounts in this case then start code into this After point
+	public void afterFinallyFindAccountAdvice(JoinPoint joinPoint) {
+		
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing Exception @After ( finally ) on method: " + method);
+
+		
+	}
+
+    // add a new advice for @AfterReturning on the findAccounts method
+	
+    @AfterThrowing(
+            pointcut = "execution(* com.aop.demo.dao.AccountDAO.findAccounts(..))",
+            throwing = "e")
+    public void afterThrowException(JoinPoint theJoinPoint, Throwable e) { 
+    	
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing Exception @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=====>>> the exception is: " + e.getMessage());
+
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.aop.demo.dao.AccountDAO.findAccounts(..))",
+            returning = "result")
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) { // bisogna asapere il tipo di ritona specifico del metodo oppure passando object si poterbbe stampare lo stesso il valore degli oggetti
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=====>>> result is: " + result);
+
+        // let's post-process the data ... let's modify it :-)
+
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+        
+        result.add(new Account());
+
+        System.out.println("\n=====>>> result is: " + result);
+
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+
+        // loop through accounts
+
+        for (Account tempAccount : result) {
+
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+        }
+    }
+
+
+    @Before("com.aop.demo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
